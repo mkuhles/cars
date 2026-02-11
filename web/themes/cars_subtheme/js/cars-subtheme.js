5bdc7c159e3ca0a22ec2a21a39f3c021f01ec8e7
Melanie Kuhles <melanie.kuhles@oder-spree-consulting.de>
2026-02-11 12:18:42 +0100

    sub theme cars

---
+++ web/themes/cars_subtheme/js/cars-subtheme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * cars-subtheme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.carsSubtheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
